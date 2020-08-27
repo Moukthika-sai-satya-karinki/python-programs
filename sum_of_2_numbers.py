9d63facafd2a0ccdabd84447745896924780f658
Moukthika-sai-satya-karinki <70310712+Moukthika-sai-satya-karinki@users.noreply.github.com>
2020-08-27 11:33:21 +0530

    Add files via upload

---
+++ sum_of_2_numbers.py
@@ -0,0 +1,5 @@
+value1=int(input('enter the first number'))
+value2=int(input('enter the second number'))
+sum= value1 + value2
+print('the sum of the given numbers is',sum)
+print('the value of',value1,'and',value2,'is',sum)
